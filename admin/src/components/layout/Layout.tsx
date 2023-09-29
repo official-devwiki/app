1999b15493c06f02e46f18b508e6d94bc8e6b5cb
dahoon06 <dahoon226@naver.com>
2023-09-29 13:44:58 +0900

    feat: add side menu

--- admin/src/components/layout/Layout.tsx
+++ admin/src/components/layout/Layout.tsx
@@ -1,9 +1,11 @@
-import { ReactElement } from 'react';
-import { Outlet } from 'react-router';
+import {ReactElement} from 'react';
+import {Outlet} from 'react-router';
+import {SideMenu} from './SideMenu';
 
 export const Layout = (): ReactElement => {
   return (
     <>
+      <SideMenu />
       <Outlet />
     </>
   );
