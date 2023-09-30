86d40d1d35b3fe811ab4fa1c2a754d59fedfd115
dahoon06 <dahoon226@naver.com>
2023-09-30 13:45:24 +0900

    feat (admin): add login page

---
+++ admin/src/components/users/SignIn.tsx
@@ -0,0 +1,50 @@
+import {ReactElement} from 'react';
+import styled from 'styled-components';
+
+const SignInForm = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const SignIn = (): ReactElement => {
+  return (
+    <SignInForm>
+      데브위키 로고
+      <div className="mb-6">
+        <label
+          htmlFor="email"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Your email
+        </label>
+        <input
+          type="email"
+          id="email"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="name@flowbite.com"
+          required
+        />
+      </div>
+      <div className="mb-6">
+        <label
+          htmlFor="password"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Your password
+        </label>
+        <input
+          type="password"
+          id="password"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          required
+        />
+      </div>
+      <button
+        type="submit"
+        className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+      >
+        Sign In
+      </button>
+    </SignInForm>
+  );
+};
--- admin/src/router/index.tsx
+++ admin/src/router/index.tsx
@@ -3,13 +3,16 @@ import {
   createRoutesFromElements,
   Route,
 } from 'react-router-dom';
-import { Layout } from '../components/layout/Layout';
-import { MainPage } from '../pages/MainPage';
+import {Layout} from '../components/layout/Layout';
+import {MainPage} from '../pages/MainPage';
+import {SignInPage} from '../pages/SignInPage';
 
+//TODO: 라우터 가드 (로그인 권한 검사)
 export const Router = () =>
   createBrowserRouter(
     createRoutesFromElements(
       <Route>
+        <Route path="sign-in" index element={<SignInPage />} />
         <Route element={<Layout />}>
           <Route index element={<MainPage />} />
         </Route>
