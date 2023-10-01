dd41a298cab0834011ef6da83de9e387100203ce
dahoon06 <dahoon226@naver.com>
2023-10-01 11:06:39 +0900

    feat: add Header Layout

--- admin/src/components/layout/Layout.tsx
+++ admin/src/components/layout/Layout.tsx
@@ -1,12 +1,32 @@
 import {ReactElement} from 'react';
 import {Outlet} from 'react-router';
 import {SideMenu} from './SideMenu';
+import styled from 'styled-components';
+import {Header} from "./Header";
 
+const headerHeight = 50;
+
+const MainLayout = styled.main`
+  margin-left: 256px;
+  height: calc(100% - ${headerHeight}px);
+`;
+
+const MainBox = styled.div`
+  background-color: white;
+  border-radius: 0 40px 0 0;
+  width: 100%;
+  height: 100%;
+`;
 export const Layout = (): ReactElement => {
   return (
     <>
       <SideMenu />
-      <Outlet />
+      <Header height={headerHeight} />
+      <MainLayout>
+        <MainBox>
+          <Outlet />
+        </MainBox>
+      </MainLayout>
     </>
   );
 };
