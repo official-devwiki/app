833b52ff76ae64e3beabd3be7e74f0f1618bba69
dahoon06 <dahoon226@naver.com>
2023-10-09 11:17:32 +0900

    fix: modal store interface type 변경

--- mollrang/src/interfaces/store.ts
+++ mollrang/src/interfaces/store.ts
@@ -6,11 +6,15 @@ export namespace Store {
    * @description Modal Interface Group
    */
   export namespace Modal {
-    type Type = 'fade' | 'bottom-slide'
+    const MODAL_TYPE = {
+      FADE: 'fade',
+      BOTTOM_SLIDE: 'bottom-slide'
+    }
+    type ModalType = typeof MODAL_TYPE[keyof typeof MODAL_TYPE];
 
     export interface State {
       type: string;
-      modalType: Type;
+      modalType: ModalType;
       isOpen: boolean;
     }
 
