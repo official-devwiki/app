99b1aa32cdc4c68e25e70f3ccc793fbd1ce4cd11
dahoon06 <dahoon226@naver.com>
2023-10-14 09:18:14 +0900

    feat (admin): mollrang quiz list 불러오기 interface 및 mock data 변경

--- admin/src/components/mollrang/quiz-lists.tsx
+++ admin/src/components/mollrang/quiz-lists.tsx
@@ -2,14 +2,34 @@ import {ReactElement} from "react";
 import styled from "styled-components";
 import {Typography} from "@components/common/typography/Typography";
 
-const mockData = [
-  { _id: '1', question: '문제 1', answer: '정답 1', prefix: '앞에', suffix: '뒤에', createdAt: 20230101 },
-  { _id: '2', question: '문제 2', answer: '정답 2', prefix: '앞에', suffix: '뒤에', createdAt: 20230102 },
-  { _id: '3', question: '문제 3', answer: '정답 3', prefix: '', suffix: '', createdAt: 20230103 },
-  { _id: '4', question: '문제 4', answer: '정답 4', prefix: '', suffix: '뒤에', createdAt: 20230104 },
-  { _id: '5', question: '문제 5', answer: '정답 5', prefix: '앞에', suffix: '뒤에', createdAt: 20230105 },
-  { _id: '6', question: '문제 6', answer: '정답 6', prefix: '앞에', suffix: '', createdAt: 20230106 },
-]
+interface ResponseData<T> {
+  success: boolean;
+  result: {
+    data: T
+  }
+}
+
+interface QuizLists {
+  question: string;
+  answer: string;
+  prefix: string;
+  suffix: string;
+  id: number;
+  askedYn: string;
+}
+
+const mockData: ResponseData<QuizLists[]> = {
+  success: true,
+  result: {
+    data: [
+      {question: '문제 1', answer: '정답 1', prefix: '앞 단어 1', suffix: '뒷 단어 1', id: 1, askedYn: 'N'},
+      {question: '문제 2', answer: '정답 2', prefix: '앞 단어 2', suffix: '뒷 단어 2', id: 2, askedYn: 'N'},
+      {question: '문제 3', answer: '정답 3', prefix: '앞 단어 3', suffix: '뒷 단어 3', id: 3, askedYn: 'N'},
+      {question: '문제 4', answer: '정답 4', prefix: '앞 단어 4', suffix: '뒷 단어 4', id: 4, askedYn: 'N'},
+      {question: '문제 5', answer: '정답 5', prefix: '앞 단어 5', suffix: '뒷 단어 5', id: 5, askedYn: 'N'},
+    ]
+  }
+}
 
 const QuizListsLayout = styled.article`
 `;
@@ -38,21 +58,19 @@ export const QuizLists = (): ReactElement => {
         <tr>
           <th>문제</th>
           <th>정답</th>
-          <th>작성일</th>
           <th>비고</th>
         </tr>
         </TableHeader>
         <TableBody>
-        {mockData.map(v => {
+        {mockData.result.data.map(v => {
           return (
-            <tr key={v._id}>
+            <tr key={v.id}>
               <td>{v.question}</td>
               <td className={'flex'}>
                 <div>{v.prefix}</div>
                 <div>{v.answer}</div>
                 <div>{v.suffix}</div>
               </td>
-              <td>{v.createdAt}</td>
               <td>
                 <button>수정</button>
                 <button>삭제</button>
