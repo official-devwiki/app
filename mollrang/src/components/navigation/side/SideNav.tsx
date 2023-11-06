74d28a4c5c0af198032d613a90a1007b4a79f04e
dahoon06 <dahoon226@naver.com>
2023-11-06 09:00:26 +0900

    design: side menu design update

--- mollrang/src/components/navigation/side/SideNav.tsx
+++ mollrang/src/components/navigation/side/SideNav.tsx
@@ -1,29 +1,13 @@
-import { ReactElement, useEffect, useRef, useState } from "react";
-import { AnimatePresence, motion } from "framer-motion";
+import { ReactElement, useEffect, useState } from "react";
+import { AnimatePresence } from "framer-motion";
 import { ModalProps } from "@components/common/modal/ModalHandler";
-import styled from "styled-components";
 import useModalHook from "@hooks/useModalHook";
 import { Button } from "@components/common/Button";
-import { XIcon } from "@components/common/icons/XIcon";
+import {GuideIcon} from "@components/common/icons/GuideIcon";
+import {Typography} from "@components/common/Typography";
+import {Icons} from "@components/common/icons/Icons";
+import * as S from './style';
 
-const SideMenuContainer = styled(motion.aside)`
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100vh;
-  z-index: 999;
-  display: flex;
-  justify-content: flex-end;
-  background-color: rgba(0, 0, 0, 0.6);
-`;
-
-const SideMenuBody = styled(motion.div)`
-  background: var(--primary);
-  height: 100%;
-  width: 300px;
-  padding: 1em;
-`;
 
 export const SideNav = (props: ModalProps): ReactElement => {
   const { isOpen, ele } = props;
@@ -38,22 +22,25 @@ export const SideNav = (props: ModalProps): ReactElement => {
   });
 
   useEffect(() => {
-    if (innerWidth <= 769) {
-      onRequestClose();
-    }
-  }, [innerWidth]);
-
-  useEffect(() => {
     const html = document.querySelector("html");
     if (html) {
       isOpen ? (html.style.overflow = "hidden") : (html.style.overflow = "");
     }
   }, [isOpen]);
 
+  const onResizeClose = () => {
+    if (innerWidth <= 769) {
+      onRequestClose();
+    }
+  }
+  useEffect(() => {
+    onResizeClose();
+  }, [innerWidth]);
+
   return (
     <AnimatePresence>
       {isOpen && (
-        <SideMenuContainer
+        <S.SideMenuContainer
           onClick={outerClickEvent}
           key={"side-nav-key"}
           initial={{ opacity: 1 }}
@@ -64,7 +51,7 @@ export const SideNav = (props: ModalProps): ReactElement => {
             opacity: 0,
           }}
         >
-          <SideMenuBody
+          <S.SideMenuBody
             initial={{ opacity: 1, x: 700 }}
             transition={{ ease: [0.17, 0.67, 0.83, 1] }}
             animate={{
@@ -75,17 +62,40 @@ export const SideNav = (props: ModalProps): ReactElement => {
               opacity: 0,
               x: 700,
             }}
+            ref={ele}
           >
-            <Button variant={"icon"} onClick={onRequestClose}>
-              <XIcon />
-            </Button>
-            <ul>
-              <li>1</li>
-              <li>2</li>
-              <li>3</li>
-            </ul>
-          </SideMenuBody>
-        </SideMenuContainer>
+            <S.ButtonWrapper>
+              <Button variant={"icon"} onClick={onRequestClose}>
+                <Icons className={'icon'} type={"exit"} />
+              </Button>
+            </S.ButtonWrapper>
+
+            <S.MenuLists>
+              <S.MenuItems>
+                <Button variant={"icon"}>
+                  <GuideIcon className={'icon'} />
+                  <Typography $variant={"caption"} as={"span"} $color={"textWhite"}>
+                    가이드
+                  </Typography>
+                </Button>
+              </S.MenuItems>
+              <S.MenuItems>
+                <Button variant={"icon"} >
+                  <Icons className={'icon'} type={"chart"} />
+                  <Typography $variant={"caption"} as={"span"} $color={"textWhite"}>
+                    통계
+                  </Typography>
+                </Button>
+              </S.MenuItems>
+              <S.MenuItems>
+                <Icons className={'icon'} type={"setting"} />
+                <Typography $variant={"caption"} as={"span"} $color={"textWhite"}>
+                  설정
+                </Typography>
+              </S.MenuItems>
+            </S.MenuLists>
+          </S.SideMenuBody>
+        </S.SideMenuContainer>
       )}
     </AnimatePresence>
   );
