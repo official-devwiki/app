cdd093a5be6fc3d5e6af5265594c566fa1b35346
dahoon06 <dahoon226@naver.com>
2023-11-08 23:36:49 +0900

    feat: pie chart label

--- mollrang/src/components/chart/PieChart.tsx
+++ mollrang/src/components/chart/PieChart.tsx
@@ -1,24 +1,26 @@
-import * as d3 from 'd3';
-import React, {ReactElement, useEffect} from 'react';
-import styled from 'styled-components';
+import * as d3 from "d3";
+import React, { ReactElement, useEffect } from "react";
+import styled from "styled-components";
 
 interface Props {
   radius?: number;
+  data: any;
 }
 
 const ChartLayout = styled.div`
   height: auto;
-  min-height: 400px;
+  min-height: 340px;
   display: flex;
   align-items: center;
-  justify-content: center
+  justify-content: center;
 `;
 
 export const PieChart = (props: Props): ReactElement => {
+  const { radius, data: chartData } = props;
 
   useEffect(() => {
-    drawChart();
-  }, []);
+    if (chartData) drawChart();
+  }, [chartData]);
 
   const drawChart = () => {
     const svgDimensions = {
@@ -26,88 +28,107 @@ export const PieChart = (props: Props): ReactElement => {
       height: 300,
     };
     const radius = Math.min(svgDimensions.width, svgDimensions.height) / 2;
-    const data: any[] = [{'number': 4, 'name': 'Locke'}, {'number': 4, 'name': 'Locke'}, {
-      'number': 4,
-      'name': 'Locke',
-    }, {'number': 4, 'name': 'Locke'}];
 
-    // const svg = d3.select(svgRef.current).attr('width', svgDimensions.width).attr('height', svgDimensions.height);
+    const data: any[] = [
+      { number: chartData["challenge1"], name: "1 번째 시도" },
+      { number: chartData["challenge2"], name: "2 번째 시도" },
+      {
+        number: chartData["challenge3"],
+        name: "3 번째 시도",
+      },
+      { number: chartData["challenge4"], name: "4 번째 시도" },
+      { number: chartData["challenge5"], name: "5 번째 시도" },
+    ];
+
     // 이전 차트 삭제
-    d3.select('#chart')
-      .select('svg') 
-      .remove();
-    
+    d3.select("#chart").select("svg").remove();
+
     const svg = d3
-      .select('#chart')
-      .append('svg')
-      .attr('width', svgDimensions.width)
-      .attr('height', svgDimensions.height);
+      .select("#chart")
+      .append("svg")
+      .attr("width", svgDimensions.width)
+      .attr("height", svgDimensions.height);
 
     const g = svg
-      .append('g')
+      .append("g")
       .attr(
-        'transform',
+        "transform",
         `translate(${svgDimensions.width / 2}, ${svgDimensions.height / 2})`,
       );
     const color = d3.scaleOrdinal([
-      '#ffb74d',
-      '#ffcc80',
-      '#ffe0b2',
-      '#fff3e0',
+      "#fbe38c",
+      "#FFE588",
+      "#fce9a6",
+      "#ffefb3",
+      "#fff4cd",
     ]);
     const pie = d3.pie().padAngle(0.1);
-    const arc: any = d3.arc().innerRadius(radius - 50).outerRadius(radius).cornerRadius(4);
+    const arc: any = d3
+      .arc()
+      .innerRadius(radius - 50)
+      .outerRadius(radius)
+      .cornerRadius(4);
 
     const arcs = g
-      .selectAll('arc')
+      .selectAll("arc")
       .data(pie.value((d: any) => d.number)(data))
       .enter()
-      .append('g')
+      .append("g")
 
-      .attr('class', 'arc')
-      .on('mouseover', onMouseOver)
-      .on('mouseout', onMouseOut);
-
-
-    arcs
-      .append('path')
-      .attr('fill', (d, i) => color(String(i)))
-      .attr('d', arc);
+      .attr("class", "arc")
+      .on("mouseover", onMouseOver)
+      .on("mouseout", onMouseOut);
 
     arcs
-      .append('text')
-      .attr('transform', (d) => `translate(${arc.centroid(d)})`)
+      .append("path")
+      .attr("fill", (d, i) => color(String(i)))
+      .attr("d", arc);
+
+    arcs
+      .append("text")
+      .attr("transform", (d) => `translate(${arc.centroid(d)})`)
       .text((d) => d.value)
-      .attr('font-family', 'sans-serif')
-      .attr('font-size', '18px')
-      .attr('font-weight', 'bold')
-      .attr('fill', '#fff')
-      .attr('text-anchor', 'middle')
-      .attr('display', 'none');
+      .attr("font-size", "12px")
+      .attr("font-weight", "bold")
+      .attr("fill", "#fff")
+      .attr("text-anchor", "middle")
+      .attr("display", "none");
 
+    arcs
+      .append("text")
+      .attr(
+        "transform",
+        (d) =>
+          `translate(${d3.arc().innerRadius(100).outerRadius(70).centroid(d)})`,
+      )
+      .text((d) => d.data.name)
+      .attr("font-size", "12px")
+      .attr("font-weight", "bold")
+      .attr("fill", "#222")
+      .attr("text-anchor", "middle");
 
     function onMouseOut(d: any, i: any) {
       d3.select(this)
-        .select('path')
+        .select("path")
         .transition()
         .duration(200)
-        .style('fill', color(i));
-      d3.select(this).select('text').attr('display', 'none');
+        .style("fill", color(i));
+      d3.select(this).select("text").attr("display", "none");
     }
 
     function onMouseOver(d: any, i: any) {
       d3.select(this)
-        .select('path')
+        .select("path")
         .transition()
         .duration(200)
-        .style('fill', 'var(--primary)');
-      d3.select(this).select('text').attr('display', 'block');
+        .style("fill", "var(--primary)");
+      d3.select(this).select("text").attr("display", "block");
     }
   };
 
   return (
     <ChartLayout>
-      <div id={'chart'} />
+      <div id={"chart"} />
     </ChartLayout>
   );
 };
