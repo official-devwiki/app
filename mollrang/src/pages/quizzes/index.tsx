a6c4ee228f99936650ad35190be9518040aec526
dahoon06 <dahoon226@naver.com>
2023-11-10 17:02:15 +0900

    # remove SSR QuizForm component test

--- mollrang/src/pages/quizzes/index.tsx
+++ mollrang/src/pages/quizzes/index.tsx
@@ -5,32 +5,32 @@ import { queryClient } from "@libs/Tanstack";
 import { QueryKeys } from "@services/keys/queryKeys";
 import { getTodayQuizzes } from "@services/apis/quizzes";
 import { dehydrate } from "@tanstack/query-core";
-// import { QuizContainer } from "@containers/quizzes";
+import { QuizContainer } from "@containers/quizzes";
 import dynamic from "next/dynamic";
 
 // @ts-ignore
-const QuizContainer = dynamic(() => import("@containers/quizzes"));
+// const QuizContainer = dynamic(() => import("@containers/quizzes"));
 
 const QuizPage: NextPage = (): ReactElement => {
   return <QuizContainer />;
 };
 export default QuizPage;
 
-export const getServerSideProps: GetServerSideProps = withGetServerSideProps(
-  async (ctx) => {
-    try {
-      await queryClient.prefetchQuery([QueryKeys.Quizzes.getTodayQuizzes], () =>
-        getTodayQuizzes(),
-      );
-      return {
-        props: {
-          dehydratedState: dehydrate(queryClient),
-        },
-      };
-    } catch (e) {
-      return {
-        props: {},
-      };
-    }
-  },
-);
+// export const getServerSideProps: GetServerSideProps = withGetServerSideProps(
+//   async (ctx) => {
+//     try {
+//       await queryClient.prefetchQuery([QueryKeys.Quizzes.getTodayQuizzes], () =>
+//         getTodayQuizzes(),
+//       );
+//       return {
+//         props: {
+//           dehydratedState: dehydrate(queryClient),
+//         },
+//       };
+//     } catch (e) {
+//       return {
+//         props: {},
+//       };
+//     }
+//   },
+// );
