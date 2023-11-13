24c69a38621f7c57992524005836bc01f257e2d4
dahoon06 <dahoon226@naver.com>
2023-11-13 14:23:29 +0900

    fix: server side prefetching

--- mollrang/src/pages/index.tsx
+++ mollrang/src/pages/index.tsx
@@ -1,9 +1,38 @@
-import { NextPage } from "next";
-import {ReactElement, useEffect} from "react";
+import {GetServerSideProps, NextPage} from "next";
+import {ReactElement} from "react";
 import { HomeContainer } from "@containers/home";
+import {dehydrate, QueryClient} from "@tanstack/react-query";
+import {QueryKeys} from "@services/keys/queryKeys";
+import {getUserAttendance} from "@services/apis/users";
+import {Attendance} from "@components/attendance/AttendanceCheck";
+import {getRandomQuiz} from "@services/apis/quizzes";
+import {Quiz} from "@interfaces/quizzes";
+import {getContinuousCorrectCount, getMyAnswersRatio} from "@services/apis/statistics";
+import withGetServerSideProps from "@utils/withGetServerSideProps";
 
 const HomePage: NextPage = (): ReactElement => {
   return <HomeContainer />;
 };
+
+export const getServerSideProps: GetServerSideProps = withGetServerSideProps(
+  async (ctx) => {
+    try {
+      const queryClient = new QueryClient();
+      await queryClient.prefetchQuery([QueryKeys.Users.getAttendance], getUserAttendance<Attendance[]>);
+      await queryClient.prefetchQuery([QueryKeys.Quizzes.getRandomQuizzes], getRandomQuiz<Quiz>);
+      await queryClient.prefetchQuery([QueryKeys.Statistics.getMyAnswers], getMyAnswersRatio<{corrected: number}>);
+      await queryClient.prefetchQuery([QueryKeys.Statistics.getContinuousCorrectCount], getContinuousCorrectCount<{continuous: number}>);
+      return {
+        props: {
+          dehydratedState: dehydrate(queryClient),
+        },
+      };
+    } catch (e) {
+      return {
+        props: {},
+      };
+    }
+  },
+);
 
 export default HomePage;
--- mollrang/src/pages/quizzes/index.tsx
+++ mollrang/src/pages/quizzes/index.tsx
@@ -1,9 +1,34 @@
-import { NextPage } from "next";
+import {GetServerSideProps, NextPage} from "next";
 import { ReactElement } from "react";
 import { QuizContainer } from "@containers/quizzes";
+import {dehydrate, QueryClient} from "@tanstack/react-query";
+import withGetServerSideProps from "@utils/withGetServerSideProps";
+import {QueryKeys} from "@services/keys/queryKeys";
+import {getTodayQuizzes} from "@services/apis/quizzes";
+import {Quiz} from "@interfaces/quizzes";
 
 const QuizPage: NextPage = (): ReactElement => {
   return <QuizContainer />;
 };
+
+export const getServerSideProps: GetServerSideProps = withGetServerSideProps(
+  async (ctx) => {
+    try {
+      const queryClient = new QueryClient();
+      await queryClient.prefetchQuery([QueryKeys.Quizzes.getTodayQuizzes], getTodayQuizzes<Quiz>);
+      return {
+        props: {
+          dehydratedState: dehydrate(queryClient),
+        },
+      };
+    } catch (e) {
+      return {
+        props: {},
+      };
+    }
+  },
+);
+
+
 export default QuizPage;
 
