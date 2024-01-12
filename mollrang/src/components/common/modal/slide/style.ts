9fca39c583447bc336b51ec2de2b08c43a609d80
dahoon06 <dahoon226@naver.com>
2024-01-13 01:45:45 +0900

    design: 모들 탑 radius 조절

--- mollrang/src/components/common/modal/slide/style.ts
+++ mollrang/src/components/common/modal/slide/style.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import {motion} from "framer-motion";
 
 export const ModalContainer = styled(motion.div)`
   width: 100%;
@@ -17,6 +17,6 @@ export const ModalBody = styled(motion.div)`
   min-height: 550px;
   height: auto;
   font-size: 4vmin;
-  border-radius: 14px 14px 0 0;
+  border-radius: 22px 22px 0 0;
   box-shadow: 1px 10px 10px -4px rgba(0, 0, 0, 0.1);
 `;
