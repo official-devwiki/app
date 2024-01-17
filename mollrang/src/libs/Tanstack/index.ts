f93e4179a5c5802a84d0034587cbf2d08d1f8e00
dahoon06 <dahoon226@naver.com>
2024-01-17 12:31:22 +0900

    fix (#28): 쿼리 데이터 유효기간 수정

--- mollrang/src/libs/Tanstack/index.ts
+++ mollrang/src/libs/Tanstack/index.ts
@@ -1,7 +1,7 @@
-import {QueryCache, QueryClient} from '@tanstack/react-query';
+import { QueryCache, QueryClient } from "@tanstack/react-query";
 
 const queryErrorHandler = (error: unknown): void => {
-  const message = error instanceof Error ? error.message : 'ERROR MESSAGE';
+  const message = error instanceof Error ? error.message : "ERROR MESSAGE";
   //TODO: ERROR 메세지 처리
 };
 
@@ -11,12 +11,10 @@ export const queryClient = new QueryClient({
   }),
   defaultOptions: {
     queries: {
-      onError: queryErrorHandler,
-      refetchOnMount: false,
-      refetchOnWindowFocus: false,
       retry: 1,
       useErrorBoundary: true,
-      cacheTime: 60 * 60 * 1000 * 24,
+      staleTime: 10000, // 10 초 이내 캐시된 결과를 사용
+      cacheTime: 60 * 1000, // 캐시 1 분
     },
     mutations: {
       useErrorBoundary: true,
